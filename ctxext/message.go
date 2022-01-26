3df7cd31b57c25b94b3278dad3979b72a8fec702
fumiama <s862105088@gmail.com>
2022-01-26 20:47:51 +0800

    ✨ add GetMessage & SendMessage

--- ctxext/message.go
+++ ctxext/message.go
@@ -7,13 +7,18 @@ import (
 	"github.com/wdvxdr1123/ZeroBot/message"
 )
 
-func GetMessage(ctx *zero.Ctx) func(int64) zero.Message {
+type (
+	NoCtxGetMsg  = func(int64) zero.Message
+	NoCtxSendMsg = func(interface{}) int64
+)
+
+func GetMessage(ctx *zero.Ctx) NoCtxGetMsg {
 	return func(id int64) zero.Message {
 		return ctx.GetMessage(message.NewMessageID(strconv.FormatInt(id, 10)))
 	}
 }
 
-func SendMessage(ctx *zero.Ctx, user int64) func(interface{}) int64 {
+func SendMessage(ctx *zero.Ctx, user int64) NoCtxSendMsg {
 	return func(msg interface{}) int64 {
 		return ctx.SendPrivateMessage(user, msg)
 	}
