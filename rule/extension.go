5ea5fbfd3f87089907d077d254752c17275573eb
fumiama <s862105088@gmail.com>
2022-01-30 19:46:27 +0800

    add ListGetter

--- rule/extension.go
+++ rule/extension.go
@@ -3,11 +3,15 @@ package rule
 
 import zero "github.com/wdvxdr1123/ZeroBot"
 
+type ListGetter interface {
+	List() []string
+}
+
 // FirstValueInList 判断正则匹配的第一个参数是否在列表中
-func FirstValueInList(list []string) zero.Rule {
+func FirstValueInList(list ListGetter) zero.Rule {
 	return func(ctx *zero.Ctx) bool {
 		first := ctx.State["regex_matched"].([]string)[1]
-		for _, v := range list {
+		for _, v := range list.List() {
 			if first == v {
 				return true
 			}
