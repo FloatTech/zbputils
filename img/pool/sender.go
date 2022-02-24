b1ad6b5fe4af33c4b7800d116efd94d523146409
fumiama <s862105088@gmail.com>
2022-02-24 12:42:45 +0800

    add pool sender

---
+++ img/pool/sender.go
@@ -0,0 +1,58 @@
+package pool
+
+import (
+	"errors"
+	"io"
+	"os"
+
+	"github.com/FloatTech/zbputils/ctxext"
+	"github.com/FloatTech/zbputils/file"
+	"github.com/FloatTech/zbputils/web"
+	"github.com/sirupsen/logrus"
+	zero "github.com/wdvxdr1123/ZeroBot"
+	"github.com/wdvxdr1123/ZeroBot/message"
+)
+
+// SendImageFromPool ...
+func SendImageFromPool(ctx *zero.Ctx, fallbackfile string, generatefallback func(io.Writer) error, send ctxext.NoCtxSendMsg, get ctxext.NoCtxGetMsg) error {
+	m, err := GetImage(fallbackfile)
+	if err != nil {
+		logrus.Debugln("[ctxext.img]", err)
+		if file.IsNotExist(fallbackfile) {
+			f, err := os.Create(fallbackfile)
+			if err != nil {
+				return err
+			}
+			err = generatefallback(f)
+			_ = f.Close()
+			if err != nil {
+				return err
+			}
+		}
+		m.SetFile(file.BOTPATH + "/" + fallbackfile)
+		hassent, err := m.Push(send, get)
+		if hassent {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+	}
+	// 发送图片
+	img := message.Image(m.String())
+	id := send(message.Message{img})
+	if id == 0 {
+		id = send(message.Message{img.Add("cache", "0")})
+		if id == 0 {
+			data, err := web.GetData(m.String())
+			if err != nil {
+				return err
+			}
+			id = send(message.Message{message.ImageBytes(data)})
+			if id == 0 {
+				return errors.New("图片发送失败，可能被风控了~")
+			}
+		}
+	}
+	return nil
+}
