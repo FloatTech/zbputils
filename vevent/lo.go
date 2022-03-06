258cc20d69cbbdf3da7d90f2fc3b6d2a16c95970
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-06 21:03:12 +0800

    update lo

--- vevent/lo.go
+++ vevent/lo.go
@@ -1,17 +1,37 @@
 package vevent
 
 import (
+	"sync"
+	"unsafe"
+
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
-type Loop struct {
-	drv zero.APICaller
+type Loop struct{ bot int64 }
+
+func NewLoop(bot int64) Loop {
+	return Loop{bot: bot}
 }
 
-func NewLoop(drv zero.APICaller) Loop {
-	return Loop{drv: drv}
+// Ctx represents the Context which hold the event.
+// 代表上下文
+type Ctx struct {
+	ma     *zero.Matcher
+	Event  *zero.Event
+	State  zero.State
+	caller zero.APICaller
+
+	// lazy message
+	once    sync.Once
+	message string
 }
 
 func (lo Loop) Echo(response []byte) {
-	processEvent(response, lo.drv)
+	zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
+		if id == lo.bot {
+			processEvent(response, (*(**Ctx)(unsafe.Pointer(&ctx))).caller)
+			return false
+		}
+		return true
+	})
 }
