b34913ac363c09155314f5e766c90e4622c03c1a
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-06 20:10:10 +0800

    add GlobalInitMutex

--- process/sleep.go
+++ process/sleep.go
@@ -3,8 +3,15 @@ package process
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
+
+// GlobalInitMutex 在 init 时被冻结，main 初始化完成后解冻
+var GlobalInitMutex = func() (mu sync.Mutex) {
+	mu.Lock()
+	return
+}()
 
 // SleepAbout1sTo2s 随机阻塞等待 1 ~ 2s
 func SleepAbout1sTo2s() {
