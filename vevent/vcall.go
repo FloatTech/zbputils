074afff73e529a2dedeac4ebffdde16fcd1cb2a1
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-07 18:20:18 +0800

    add APICallerHook

---
+++ vevent/vcall.go
@@ -0,0 +1,25 @@
+package vevent
+
+import (
+	"unsafe"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+)
+
+type APICallerHook struct {
+	caller   zero.APICaller
+	callback func(rsp zero.APIResponse, err error)
+}
+
+func NewAPICallerHook(ctx *zero.Ctx, callback func(rsp zero.APIResponse, err error)) (v *APICallerHook) {
+	return &APICallerHook{
+		caller:   (*(**Ctx)(unsafe.Pointer(&ctx))).caller,
+		callback: callback,
+	}
+}
+
+func (v *APICallerHook) CallApi(request zero.APIRequest) (rsp zero.APIResponse, err error) {
+	rsp, err = v.caller.CallApi(request)
+	v.callback(rsp, err)
+	return
+}
