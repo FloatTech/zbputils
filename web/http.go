f542a820183cd8b6b2f365845e3e17b39f6fe327
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-13 13:06:47 +0800

    更改getdatawith

--- web/http.go
+++ web/http.go
@@ -82,15 +82,19 @@ func NewPixivClient() *http.Client {
 	}
 }
 
-// GetDataWith 使用自定义请求头获取数据
-func GetDataWith(client *http.Client, url, referer, ua string) (data []byte, err error) {
+// RequestDataWith 使用自定义请求头获取数据
+func RequestDataWith(client *http.Client, url, method, referer, ua string) (data []byte, err error) {
 	// 提交请求
 	var request *http.Request
-	request, err = http.NewRequest("GET", url, nil)
+	request, err = http.NewRequest(method, url, nil)
 	if err == nil {
 		// 增加header选项
-		request.Header.Add("Referer", referer)
-		request.Header.Add("User-Agent", ua)
+		if referer != "" {
+			request.Header.Add("Referer", referer)
+		}
+		if ua != "" {
+			request.Header.Add("User-Agent", ua)
+		}
 		var response *http.Response
 		response, err = client.Do(request)
 		if err == nil {
