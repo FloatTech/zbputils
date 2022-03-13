7fc7fa20d8aa4c954022dc044a9428fc4bf8c100
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-13 13:18:45 +0800

    fix: missing port in address

--- web/dns.go
+++ web/dns.go
@@ -16,7 +16,7 @@ var (
 	// 定义resolver
 	resolver = &net.Resolver{
 		Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-			return dialer.DialContext(ctx, "tcp", "8.8.8.8") // 通过tcp请求nameserver解析域名
+			return dialer.DialContext(ctx, "tcp", "8.8.8.8:53") // 通过tcp请求nameserver解析域名
 		},
 	}
 
