95033087f2017efae89868d23809f38985d4ecf4
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-03-27 13:26:38 +0800

    add fonts

--- img/text/font.go
+++ img/text/font.go
@@ -7,8 +7,14 @@ const (
 	FontFile = FontPath + "regular.ttf"
 	// BoldFontFile 粗体苹方字体
 	BoldFontFile = FontPath + "regular-bold.ttf"
-	// SakuraFontFile ...
+	// SakuraFontFile 樱酥体
 	SakuraFontFile = FontPath + "sakura.ttf"
 	// ConsolasFontFile ...
 	ConsolasFontFile = FontPath + "consolas.ttf"
+	// SyumatuFontFile 少女终末旅行字体
+	SyumatuFontFile = FontPath + "zhongmoti.ttf"
+	// NisiFontFile 少女终末旅行字体变体
+	NisiFontFile = FontPath + "Nisi-Regular.ttf"
+	// VioletEvergardenFontFile 紫罗兰永恒花园字体 https://github.com/kagurazakayashi/EvergardenConverter
+	VioletEvergardenFontFile = FontPath + "VioletEvergardenFont.ttf"
 )
