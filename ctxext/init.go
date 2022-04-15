74a07dd318ad06dee1f6a90b55aab6f43625f6c7
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-04-15 23:27:25 +0800

    优化代码结构

---
+++ ctxext/init.go
@@ -0,0 +1,21 @@
+package ctxext
+
+import (
+	"github.com/FloatTech/zbputils/process"
+	zero "github.com/wdvxdr1123/ZeroBot"
+)
+
+// DoOnceOnSuccess 当返回 true，之后直接通过，否则下次触发仍会执行
+func DoOnceOnSuccess(f zero.Rule) zero.Rule {
+	init := process.NewOnce()
+	return func(ctx *zero.Ctx) (success bool) {
+		init.Do(func() {
+			success = f(ctx)
+		})
+		if !success {
+			init.Reset()
+			return false
+		}
+		return true
+	}
+}
