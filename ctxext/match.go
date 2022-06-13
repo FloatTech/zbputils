33e22060e8d99575b64fa7bcada66491a0529140
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-06-13 12:28:33 +0800

    fix value in list

--- ctxext/match.go
+++ ctxext/match.go
@@ -5,8 +5,9 @@ type ListGetter interface {
 }
 
 // ValueInList 判断参数是否在列表中
-func ValueInList[Ctx any](val string, list ListGetter) func(Ctx) bool {
+func ValueInList[Ctx any](getval func(Ctx) string, list ListGetter) func(Ctx) bool {
 	return func(ctx Ctx) bool {
+		val := getval(ctx)
 		for _, v := range list.List() {
 			if val == v {
 				return true
