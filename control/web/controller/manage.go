90e072c406eaf1170e6ebb6a1264b27ce3c9e04d
himawari <1156544355@qq.com>
2023-03-13 17:26:34 +0800

    🎨 优化swagger

--- control/web/controller/manage.go
+++ control/web/controller/manage.go
@@ -76,39 +76,43 @@ func init() {
 }
 
 // logWriter
-type logWriter struct {
-	// 日志连接
-}
+type logWriter struct{}
 
 // GetBotList 获取机器人qq号
+// @Tags    通用
+// @Summary 获取机器人qq号
 // @Description 获取机器人qq号
 // @Router /api/getBotList [get]
+// @Success 200 {object} types.Response "成功"
 func GetBotList(context *gin.Context) {
 	var bots []int64
 	zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
 		bots = append(bots, id)
 		return true
 	})
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  bots,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       bots,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetFriendList 获取好友列表
+// @Tags    通用
+// @Summary 获取好友列表
 // @Description 获取好友列表
 // @Router /api/getFriendList [get]
-// @Param selfId query integer false "机器人qq号" default(123456)
+// @Param object body types.BotParams false "获取好友列表入参"
+// @Success 200 {object} types.Response "成功"
 func GetFriendList(context *gin.Context) {
 	_, bot, err := getBot(context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -116,34 +120,37 @@ func GetFriendList(context *gin.Context) {
 	list := bot.GetFriendList().String()
 	err = json.Unmarshal(binary.StringToBytes(list), &resp)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  resp,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       resp,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetGroupList 获取群列表
+// @Tags    通用
+// @Summary 获取群列表
 // @Description 获取群列表
 // @Router /api/getGroupList [get]
-// @Param selfId query integer false "机器人qq号" default(123456)
+// @Param object body types.BotParams false "获取群列表入参"
+// @Success 200 {object} types.Response "成功"
 func GetGroupList(context *gin.Context) {
 	_, bot, err := getBot(context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -151,35 +158,86 @@ func GetGroupList(context *gin.Context) {
 	list := bot.GetGroupList().String()
 	err = json.Unmarshal(binary.StringToBytes(list), &resp)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  resp,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       resp,
+		Message:      "",
+		ResponseType: "ok",
+	})
+}
+
+// DeleteGroup 删除群聊或好友
+// @Tags    通用
+// @Summary 删除群聊或好友
+// @Description 删除群聊或好友
+// @Router /api/deleteGroup [get]
+// @Param object body types.DeleteGroupParams false "删除群聊或好友入参"
+// @Success 200 {object} types.Response "成功"
+func DeleteGroup(context *gin.Context) {
+	var (
+		d   types.DeleteGroupParams
+		bot *zero.Ctx
+		gid int64
+	)
+	err := bind(&d, context)
+	if err != nil {
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
+		})
+		return
+	}
+	if d.SelfID == 0 {
+		zero.RangeBot(func(id int64, ctx *zero.Ctx) bool {
+			bot = ctx
+			return false
+		})
+	} else {
+		bot = zero.GetBot(d.SelfID)
+	}
+	if d.GroupID >= 0 {
+		gid = d.GroupID
+		bot.SetGroupLeave(gid, false)
+	} else {
+		gid = -d.GroupID
+		bot.CallAction("delete_friend", zero.Params{
+			"user_id": gid,
+		})
+	}
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetAllPlugin 获取所有插件的状态
+// @Tags    插件
+// @Summary 获取所有插件的状态
 // @Description 获取所有插件的状态
 // @Router /api/manage/getAllPlugin [get]
-// @Param groupId query integer false "群号" default(0)
+// @Param object body types.AllPluginParams false "获取所有插件的状态入参"
+// @Success 200 {object} types.Response{result=[]types.PluginVo} "成功"
 func GetAllPlugin(context *gin.Context) {
 	var d types.AllPluginParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -197,38 +255,40 @@ func GetAllPlugin(context *gin.Context) {
 		pluginVoList = append(pluginVoList, p)
 		return true
 	})
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  pluginVoList,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       pluginVoList,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetPlugin 获取某个插件的状态
+// @Tags    插件
+// @Summary 获取某个插件的状态
 // @Description 获取某个插件的状态
 // @Router /api/manage/getPlugin [get]
-// @Param groupId query integer false "群号" default(0)
-// @Param name query string false "插件名" default(antibuse)
+// @Param object body types.PluginParams false "获取某个插件的状态入参"
+// @Success 200 {object} types.Response{result=types.PluginVo} "成功"
 func GetPlugin(context *gin.Context) {
 	var d types.PluginParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
 	con, b := control.Lookup(d.Name)
 	if !b {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": d.Name + "服务不存在",
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      d.Name + "服务不存在",
+			ResponseType: "error",
 		})
 		return
 	}
@@ -240,37 +300,40 @@ func GetPlugin(context *gin.Context) {
 		PluginStatus:   con.IsEnabledIn(d.GroupID),
 		ResponseStatus: control.CanResponse(d.GroupID),
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  p,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       p,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // UpdatePluginStatus 更改某一个插件状态
+// @Tags    插件
+// @Summary 更改某一个插件状态
 // @Description 更改某一个插件状态
 // @Router /api/manage/updatePluginStatus [post]
 // @Param object body types.PluginStatusParams false "修改插件状态入参"
+// @Success 200 {object} types.Response "成功"
 func UpdatePluginStatus(context *gin.Context) {
 	var d types.PluginStatusParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
 	con, b := control.Lookup(d.Name)
 	if !b {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": d.Name + "服务不存在",
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      d.Name + "服务不存在",
+			ResponseType: "error",
 		})
 		return
 	}
@@ -283,27 +346,30 @@ func UpdatePluginStatus(context *gin.Context) {
 		con.Reset(d.GroupID)
 	default:
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  nil,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // UpdateResponseStatus 更改某一个群响应
+// @Tags    响应
+// @Summary 更改某一个群响应
 // @Description 更改某一个群响应
 // @Router /api/manage/updateResponseStatus [post]
 // @Param object body types.ResponseStatusParams false "修改群响应入参"
+// @Success 200 {object} types.Response "成功"
 func UpdateResponseStatus(context *gin.Context) {
 	var d types.ResponseStatusParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -313,63 +379,74 @@ func UpdateResponseStatus(context *gin.Context) {
 		err = control.Silence(d.GroupID)
 	}
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  nil,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // UpdateAllPluginStatus 更改某群所有插件状态
+// @Tags    响应
+// @Summary 更改某群所有插件状态
 // @Description 更改某群所有插件状态
 // @Router /api/manage/updateAllPluginStatus [post]
 // @Param object body types.AllPluginStatusParams false "修改插件状态入参"
+// @Success 200 {object} types.Response "成功"
 func UpdateAllPluginStatus(context *gin.Context) {
 	var d types.AllPluginStatusParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
-	if d.Status == 1 {
-		control.ForEachByPrio(func(i int, manager *ctrl.Control[*zero.Ctx]) bool {
-			manager.Enable(d.GroupID)
-			return true
-		})
-	} else {
+	switch d.Status {
+	case 0:
 		control.ForEachByPrio(func(i int, manager *ctrl.Control[*zero.Ctx]) bool {
 			manager.Disable(d.GroupID)
 			return true
 		})
+	case 1:
+		control.ForEachByPrio(func(i int, manager *ctrl.Control[*zero.Ctx]) bool {
+			manager.Enable(d.GroupID)
+			return true
+		})
+	case 2:
+		control.ForEachByPrio(func(i int, manager *ctrl.Control[*zero.Ctx]) bool {
+			manager.Reset(d.GroupID)
+			return true
+		})
+	default:
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  nil,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // HandleRequest 处理一个请求
+// @Tags    通用
+// @Summary 处理一个请求
 // @Description 处理一个请求
 // @Router /api/handleRequest [post]
-// @Param flag formData string true "事件id" default(abc)
-// @Param reason formData string false "原因" default(abc)
-// @Param approve formData bool false "是否同意" default(true)
+// @Param object body types.HandleRequestParams false "处理一个请求入参"
+// @Success 200 {object} types.Response "成功"
 func HandleRequest(context *gin.Context) {
 	var (
 		d        types.HandleRequestParams
@@ -377,21 +454,21 @@ func HandleRequest(context *gin.Context) {
 	)
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
 	r, ok := requestData.LoadAndDelete(d.Flag)
 	if !ok {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": "flag not found",
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      "flag not found",
+			ResponseType: "error",
 		})
 		return
 	}
@@ -408,26 +485,29 @@ func HandleRequest(context *gin.Context) {
 		}
 	}
 	log.Debugln("[gui] 已处理", r.UserID, "的"+typeName)
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  nil,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetRequestList 获取所有的请求
+// @Tags    通用
+// @Summary 获取所有的请求
 // @Description 获取所有的请求
 // @Router /api/getRequestList [get]
-// @Param selfId query integer false "机器人qq号" default(123456)
+// @Param object body types.BotParams false "获取所有的请求入参"
+// @Success 200 {object} types.Response{result=[]types.RequestVo} "成功"
 func GetRequestList(context *gin.Context) {
 	d, bot, err := getBot(context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -449,11 +529,11 @@ func GetRequestList(context *gin.Context) {
 		})
 		return true
 	})
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  data,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       data,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
@@ -476,9 +556,12 @@ func Upgrade(context *gin.Context) {
 }
 
 // SendMsg 前端调用发送信息
+// @Tags    通用
+// @Summary 前端调用发送信息
 // @Description 前端调用发送信息
 // @Router /api/sendMsg [post]
 // @Param object body types.SendMsgParams false "发消息参数"
+// @Success 200 {object} types.Response{result=int} "成功"
 func SendMsg(context *gin.Context) {
 	var (
 		d     types.SendMsgParams
@@ -486,11 +569,11 @@ func SendMsg(context *gin.Context) {
 	)
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -502,11 +585,11 @@ func SendMsg(context *gin.Context) {
 			msgID = bot.SendPrivateMessage(-gid, message.UnescapeCQCodeText(d.Message))
 		}
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  msgID,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       msgID,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
@@ -521,30 +604,32 @@ func (l logWriter) Write(p []byte) (n int, err error) {
 	return len(p), nil
 }
 
-// Login 登录接口
+// Login 前端登录
+// @Tags    用户
+// @Summary 前端登录
 // @Description 前端登录
 // @Router /api/login [post]
-// @Param username formData string true "用户名" default(xiaoguofan)
-// @Param password formData string true "密码" default(123456)
+// @Param object body types.LoginParams false "前端登录"
+// @Success 200 {object} types.Response{result=types.LoginResultVo} "成功"
 func Login(context *gin.Context) {
 	var d types.LoginParams
 	err := bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
 	user, err := model.FindUser(d.Username, d.Password)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
@@ -563,17 +648,20 @@ func Login(context *gin.Context) {
 		UserID:   int(user.ID),
 		Username: user.Username,
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  r,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       r,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetUserInfo 获得用户信息
+// @Tags    用户
+// @Summary 获得用户信息
 // @Description 获得用户信息
 // @Router /api/getUserInfo [get]
+// @Success 200 {object} types.Response{result=types.UserInfoVo} "成功"
 func GetUserInfo(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 	i, _ := middleware.LoginCache.Get(token)
@@ -595,50 +683,56 @@ func GetUserInfo(context *gin.Context) {
 		Username: user.Username,
 		Avatar:   "https://q1.qlogo.cn/g?b=qq&nk=" + strconv.FormatInt(qq, 10) + "&s=640",
 	}
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  r,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       r,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // Logout 登出
+// @Tags    用户
+// @Summary 登出
 // @Description 登出
 // @Router /api/logout [get]
+// @Success 200 {object} types.Response "成功"
 func Logout(context *gin.Context) {
 	token := context.Request.Header.Get("Authorization")
 	middleware.LoginCache.Delete(token)
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  nil,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       nil,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 // GetPermCode 授权码
+// @Tags    用户
+// @Summary 授权码
 // @Description 授权码
 // @Router /api/getPermCode [get]
+// @Success 200 {object} types.Response{result=[]string} "成功"
 func GetPermCode(context *gin.Context) {
 	r := []string{"1000", "3000", "5000"}
 	// 先写死接口
-	context.JSON(http.StatusOK, gin.H{
-		"code":    0,
-		"result":  r,
-		"message": "",
-		"type":    "ok",
+	context.JSON(http.StatusOK, types.Response{
+		Code:         0,
+		Result:       r,
+		Message:      "",
+		ResponseType: "ok",
 	})
 }
 
 func getBot(context *gin.Context) (d types.BotParams, bot *zero.Ctx, err error) {
 	err = bind(&d, context)
 	if err != nil {
-		context.JSON(http.StatusOK, gin.H{
-			"code":    -1,
-			"result":  nil,
-			"message": err.Error(),
-			"type":    "error",
+		context.JSON(http.StatusOK, types.Response{
+			Code:         -1,
+			Result:       nil,
+			Message:      err.Error(),
+			ResponseType: "error",
 		})
 		return
 	}
--- control/web/gui.go
+++ control/web/gui.go
@@ -60,7 +60,7 @@ func RunGui(addr string) {
 		err := recover()
 		if err != nil {
 			log.Errorln("[gui] ZeroBot-Plugin-Webui出现不可恢复的错误")
-			log.Errorln("[gui] err:", err, ",stack:", debug.Stack())
+			log.Errorln("[gui] err:", err, ", stack:", debug.Stack())
 		}
 	}()
 
