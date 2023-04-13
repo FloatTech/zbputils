3e45c1646d3ca2a3dd287f2c286f57962cc0e954
himawari <1156544355@qq.com>
2023-04-13 21:48:36 +0800

    🎨 修改字符类型

--- job/web.go
+++ job/web.go
@@ -14,8 +14,8 @@ import (
 )
 
 type (
-	// JobType 任务类型,1=指令别名,2=定时任务,3=你问我答
-	JobType uint8
+	// Type 任务类型,1=指令别名,2=定时任务,3=你问我答
+	Type uint8
 	// FullMatchType 指令别名类型, jobType=1使用的参数, 1=无状态消息, 2=主人消息
 	FullMatchType uint8
 	// QuestionType 问题类型, jobType=3使用的参数, 1=单独问, 2=所有人问
@@ -26,7 +26,7 @@ type (
 
 const (
 	// FullMatchJob 指令别名
-	FullMatchJob JobType = iota + 1
+	FullMatchJob Type = iota + 1
 	// CronJob 定时任务
 	CronJob
 	// RegexpJob 你问我答
@@ -60,7 +60,7 @@ const (
 type Job struct {
 	ID            string        `json:"id"`            // 任务id
 	SelfID        int64         `json:"selfId"`        // 机器人id
-	JobType       JobType       `json:"jobType"`       // 任务类型,1-指令别名,2-定时任务,3-你问我答
+	JobType       Type          `json:"jobType"`       // 任务类型,1-指令别名,2-定时任务,3-你问我答
 	Matcher       string        `json:"matcher"`       // 当jobType=1时 为指令别名,当jobType=2时 为cron表达式,当jobType=3时 为正则表达式
 	Handler       string        `json:"handler"`       // 执行内容
 	FullMatchType FullMatchType `json:"fullMatchType"` // 指令别名类型, jobType=1使用的参数, 1=无状态消息, 2=主人消息
