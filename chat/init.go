5d403aa9cecf53ddef5b6ce0beba1ed499f87179
源文雨 <41315874+fumiama@users.noreply.github.com>
2025-02-22 14:58:44 +0900

    feat(chat): new template

--- chat/init.go
+++ chat/init.go
@@ -1,13 +1,19 @@
 package chat
 
 import (
+	_ "embed"
+
 	"github.com/fumiama/deepinfra"
+	"github.com/fumiama/deepinfra/chat"
 	"github.com/fumiama/deepinfra/model"
 
 	zero "github.com/wdvxdr1123/ZeroBot"
 )
 
-var lst = newlist()
+//go:embed README.md
+var SystemPrompt string
+
+var lst = chat.NewLog(8, "\n\n", "自己随机开启新话题", "【", "】", ">>")
 
 func init() {
 	zero.OnMessage(func(ctx *zero.Ctx) bool {
@@ -19,14 +25,18 @@ func init() {
 		if gid == 0 {
 			gid = -ctx.Event.UserID
 		}
-		lst.add(gid, ctx.Event.Sender.Name(), ctx.State["__zbputil_chat_txt__"].(string), false)
+		lst.Add(
+			gid, ctx.Event.Sender.Name(),
+			ctx.State["__zbputil_chat_txt__"].(string),
+			false, ctx.Event.IsToMe,
+		)
 	})
 }
 
 func Reply(grp int64, txt string) {
-	lst.add(grp, "", txt, true)
+	lst.Add(grp, "", txt, true, false)
 }
 
 func Ask(p model.Protocol, grp int64, sysp string) deepinfra.Model {
-	return lst.modelize(p, grp, sysp)
+	return lst.Modelize(p, grp, sysp)
 }
