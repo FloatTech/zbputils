f24966af2495494ebf3b69b4d4514633fb0a5ba0
源文雨 <41315874+fumiama@users.noreply.github.com>
2025-09-21 22:22:15 +0800

    feat(chat): add agent mode

--- chat/agent.go
+++ chat/agent.go
@@ -1,3 +1,90 @@
 package chat
 
-//TODO: impl.
+import (
+	"bytes"
+	_ "embed"
+
+	"github.com/RomiChan/syncx"
+	goba "github.com/fumiama/go-onebot-agent"
+	"gopkg.in/yaml.v3"
+
+	zero "github.com/wdvxdr1123/ZeroBot"
+)
+
+// AgentChar 将 char.yaml 内容嵌入为默认 agent 性格
+//
+//go:embed char.yaml
+var AgentChar []byte
+
+var ags = syncx.Map[int64, *goba.Agent]{}
+
+type charcfg struct {
+	Sex     string `yaml:"sex"`
+	Char    string `yaml:"char"`
+	Default string `yaml:"default"`
+}
+
+// AgentOf id is self_id
+func AgentOf(id int64) *goba.Agent {
+	if ag, ok := ags.Load(id); ok {
+		return ag
+	}
+	var cfg charcfg
+	err := yaml.NewDecoder(bytes.NewReader(AgentChar)).Decode(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	ag := goba.NewAgent(
+		id, 16, 8, zero.BotConfig.NickName[0],
+		cfg.Sex, cfg.Char, cfg.Default,
+	)
+	ags.Store(id, &ag)
+	return &ag
+}
+
+func togobaev(ev *zero.Event) *goba.Event {
+	msgid := int64(0)
+	if id, ok := ev.MessageID.(int64); ok {
+		msgid = id
+	} else {
+		return nil
+	}
+	return &goba.Event{
+		Time:        ev.Time,
+		PostType:    ev.PostType,
+		MessageType: ev.MessageType,
+		SubType:     ev.SubType,
+		MessageID:   msgid,
+		GroupID:     ev.GroupID,
+		UserID:      ev.UserID,
+		TargetID:    ev.TargetID,
+		SelfID:      ev.SelfID,
+		NoticeType:  ev.NoticeType,
+		OperatorID:  ev.OperatorID,
+		File:        ev.File,
+		RequestType: ev.RequestType,
+		Flag:        ev.Flag,
+		Comment:     ev.Comment,
+		Sender:      ev.Sender,
+		Message:     ev.NativeMessage,
+	}
+}
+
+func logev(ctx *zero.Ctx) {
+	// 计算群组 ID（私聊时使用负的 UserID）
+	gid := ctx.Event.GroupID
+	if gid == 0 {
+		gid = -ctx.Event.UserID
+	}
+	ev := togobaev(ctx.Event)
+	if ev == nil {
+		return
+	}
+	AgentOf(ctx.Event.SelfID).AddEvent(gid, ev)
+}
+
+func init() {
+	zero.OnNotice().FirstPriority().SetBlock(false).Handle(logev)
+	zero.OnMessage().FirstPriority().SetBlock(false).Handle(logev)
+	zero.OnRequest().FirstPriority().SetBlock(false).Handle(logev)
+}
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/fumiama/cron v1.3.0
 	github.com/fumiama/deepinfra v0.0.0-20250920170049-e3d1b92cc3a1
 	github.com/fumiama/go-base16384 v1.7.0
+	github.com/fumiama/go-onebot-agent v0.0.0-20250921065603-7f39bcf97166
 	github.com/gin-gonic/gin v1.8.2
 	github.com/go-playground/assert/v2 v2.2.0
 	github.com/go-playground/validator/v10 v10.11.1
@@ -25,6 +26,7 @@ require (
 	github.com/swaggo/swag v1.8.10
 	github.com/tidwall/gjson v1.18.0
 	github.com/wdvxdr1123/ZeroBot v1.8.2-0.20250921063512-13752a73d444
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -60,6 +62,7 @@ require (
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/pbnjay/memory v0.0.0-20210728143218-7b4eea64cf58 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
